3e9b5d8b9167af06cc2ef5489b145e968d78a46a
zhengxyou <Qq26861130>
2018-11-04 00:13:12 +0800

    添加根据文件Uri获取真实地址的工具类

---
+++ commonlibrary/src/main/java/com/zhengxyou/commonlibrary/utils/FileUriUtils.java
@@ -0,0 +1,179 @@
+package com.zhengxyou.commonlibrary.utils;
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author zhengxyou
+ * 用于根据文件Uri获取真实地址
+ * {link "https://blog.csdn.net/smileiam/article/details/79753745"}
+ */
+public class FileUriUtils {
+
+    public static String getFilePath(@NonNull Context context, @NonNull Uri uri) {
+        // 以 file:// 开头的
+        if (ContentResolver.SCHEME_FILE.equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && DocumentsContract.isDocumentUri(context, uri)) {
+            return getPath(context, uri);
+        } else if (ContentResolver.SCHEME_CONTENT.equals(uri.getScheme())) {
+            if (isGooglePhotosUri(uri)) {//判断是否是google相册图片
+                return uri.getLastPathSegment();
+            } else if (isGooglePlayPhotosUri(uri)) {//判断是否是Google相册图片
+                return getImageUrlWithAuthority(context, uri);
+            } else {//其他类似于media这样的图片，和android4.4以下获取图片path方法类似
+                //适配api19以下
+                return getDataColumn(context, uri, null, null);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 适配api19及以上,根据uri获取图片的绝对路径
+     *
+     * @param context 上下文对象
+     * @param uri     文件的Uri
+     * @return 如果Uri对应的文件存在, 那么返回该文件的绝对路径, 否则返回null
+     */
+    private static String getPath(Context context, Uri uri) {
+        String docId = DocumentsContract.getDocumentId(uri);
+        if (isExternalStorageDocument(uri)) {
+            String[] split = docId.split(":");
+            if (split.length >= 2) {
+                String type = split[0];
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+            }
+        } else if (isDownloadsDocument(uri)) {
+            Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(docId));
+            return getDataColumn(context, contentUri, null, null);
+        } else if (isMediaDocument(uri)) {
+            String[] split = docId.split(":");
+            if (split.length >= 2) {
+                String type = split[0];
+                Uri contentUris = null;
+                if ("image".equals(type)) {
+                    contentUris = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUris = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUris = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+                String selection = MediaStore.Images.Media._ID + "=?";
+                String[] selectionArgs = new String[]{split[1]};
+                return getDataColumn(context, contentUris, selection, selectionArgs);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Google相册图片获取路径
+     **/
+    private static String getImageUrlWithAuthority(Context context, Uri uri) {
+        InputStream is = null;
+        if (uri.getAuthority() != null) {
+            try {
+                is = context.getContentResolver().openInputStream(uri);
+                Bitmap bmp = BitmapFactory.decodeStream(is);
+                return writeToTempImageAndGetPathUri(context, bmp).toString();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                if (is != null) {
+                    try {
+                        is.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 将图片流读取出来保存到手机本地相册中
+     **/
+    private static Uri writeToTempImageAndGetPathUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "tempImage", null);
+        return Uri.parse(path);
+    }
+
+    /**
+     * @param uri the Uri to check
+     * @return Whether the Uri authority is MediaProvider
+     */
+    private static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri the Uri to check
+     * @return Whether the Uri authority is DownloadsProvider
+     */
+    private static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * 判断是否是Google相册的图片，类似于content://com.google.android.apps.photos.contentprovider/0/1/mediakey:/local%3A821abd2f-9f8c-4931-bbe9-a975d1f5fabc/ORIGINAL/NONE/1075342619
+     **/
+    public static boolean isGooglePlayPhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.contentprovider".equals(uri.getAuthority());
+    }
+
+    /**
+     * 判断是否是Google相册的图片，类似于content://com.google.android.apps.photos.content/...
+     **/
+    public static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+    }
+
+    private static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * 获取数据库表中的 _data 列，即返回Uri对应的文件路径
+     */
+    private static String getDataColumn(Context context, Uri uri, String selection, String[]
+            selectionArgs) {
+        String path = null;
+
+        String[] projection = new String[]{MediaStore.Images.Media.DATA};
+        Cursor cursor = null;
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                int columnIndex = cursor.getColumnIndexOrThrow(projection[0]);
+                path = cursor.getString(columnIndex);
+            }
+        } catch (Exception e) {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+        return path;
+    }
+}
